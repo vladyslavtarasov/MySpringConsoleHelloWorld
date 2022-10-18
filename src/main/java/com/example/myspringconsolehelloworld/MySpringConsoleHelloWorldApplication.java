dd7a1a222d4e81d285397e97ee6e7029cd7a1daa
Vladyslav Tarasov <vladyslavtarasov02@gmail.com>
2022-10-18 15:38:26 +0300

    Initial commit

---
+++ src/main/java/com/example/myspringconsolehelloworld/MySpringConsoleHelloWorldApplication.java
@@ -0,0 +1,22 @@
+package com.example.myspringconsolehelloworld;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.annotation.Order;
+
+@SpringBootApplication
+@Order(2)
+public class MySpringConsoleHelloWorldApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		System.out.println("Begin of main");
+		SpringApplication.run(MySpringConsoleHelloWorldApplication.class, args);
+		System.out.println("End of main");
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello from Spring Boot!");
+	}
+}
---
+++ src/test/java/com/example/myspringconsolehelloworld/MySpringConsoleHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myspringconsolehelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringConsoleHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
